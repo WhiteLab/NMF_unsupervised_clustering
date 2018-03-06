5c19dfadd6e8f632ee516386fa3558e12e7e6892
migbro <miguel.a.brown@gmail.com>
2018-03-06 11:45:34 -0600

    edited to print ALL labels

--- consensus_matrix.py
+++ consensus_matrix.py
@@ -71,7 +71,7 @@ def visualizeConsensus(consensusMat, connectivityMatrices, clusters, colNames, s
         # dataframe.to_csv(str(matrixPath + 'consensus_matrix_table.txt'), sep="\t")
         # clusters by columns and rows and annotates probablility a particular sample clusters together
         # cluster distance is meausred by average Euclidean Distance in seaborn for hierarchical clustering
-        consensusClustered = sns.clustermap(dataframe, annot=True, rasterized=True)
+        consensusClustered = sns.clustermap(dataframe, annot=True)
         ax = consensusClustered.ax_heatmap
         xaxis = []
         for ind in consensusClustered.dendrogram_col.reordered_ind:
@@ -82,7 +82,7 @@ def visualizeConsensus(consensusMat, connectivityMatrices, clusters, colNames, s
             yaxis.append(sampleNames[ind])
         ax.set_yticklabels(yaxis, rotation=0)
         # consensusClustered.data.to_csv(str(matrixPath + 'consensus_matrix_table_clustered.txt'), sep="\t")
-        consensusClustered_non_annt = sns.clustermap(dataframe, annot=False, rasterized=True)
+        consensusClustered_non_annt = sns.clustermap(dataframe, annot=False)
         axNA = consensusClustered_non_annt.ax_heatmap
         xaxis = []
         for ind in consensusClustered_non_annt.dendrogram_col.reordered_ind:
