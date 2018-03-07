1eaf20540075542178ff6983df265efefc79a4d4
migbro <miguel.a.brown@gmail.com>
2018-03-07 15:29:58 -0600

    Added script to tabulate min KL for each cluster for elbow method of determining optimal number of clusters

---
+++ tabulate_mean_min_kl_clust.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+from statistics import mean
+c_dict = {}
+
+for line in open(sys.argv[1]):
+    fn = line.rstrip('\n')
+    cur = open(fn)
+    next(cur)
+    cnum = next(cur)
+    c_info = cnum.rstrip('\n').split()
+    num_k = c_info[1]
+    if num_k not in c_dict:
+        c_dict[num_k] = []
+    next(cur)
+    next(cur)
+    min_k = next(cur)
+    min_info = min_k.rstrip('\n').split()
+    c_dict[num_k].append(float(min_info[1]))
+    cur.close()
+
+print('Cluster\tmin KL')
+for k in c_dict:
+    print(k + '\t' + str(mean(c_dict[k])))
